471086a008e2f2019e9ee193c08be8d2619feb13
GustavoAngel <gustavoandradeangel@hotmail.com>
2025-10-22 20:35:57 -0600

    operativo

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,13 @@
-const CACHE_NAME = 'inventario-pwa-cache-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/app.js',
-    '/manifest.json'
-];
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-    );
+// service-worker.js
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/css/styles.css',
+        '/js/app.js'
+      ]);
+    })
+  );
 });
